95ec80ec2757625407b8c1ed0641adb1389dad48
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-03-23 12:28:09 +0530

    Teachers Dashboard updated

--- Attendence_System/app/src/main/java/com/starkindustries/attendence_system/Teachers_DashBoard.java
+++ Attendence_System/app/src/main/java/com/starkindustries/attendence_system/Teachers_DashBoard.java
@@ -1,17 +1,22 @@
 package com.starkindustries.attendence_system;
+import androidx.annotation.Keep;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
+import com.starkindustries.attendence_system.Keys.Keys;
 import com.starkindustries.attendence_system.databinding.ActivityTeachersDashBoardBinding;
 
 public class Teachers_DashBoard extends AppCompatActivity {
     public ActivityTeachersDashBoardBinding binding;
+    public SharedPreferences preferences;
+    public SharedPreferences.Editor edit;
     public FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,14 @@ public class Teachers_DashBoard extends AppCompatActivity {
         setContentView(R.layout.activity_teachers_dash_board);
         binding= DataBindingUtil.setContentView(Teachers_DashBoard.this,R.layout.activity_teachers_dash_board);
         auth= FirebaseAuth.getInstance();
+        preferences=getSharedPreferences(Keys.SHARED_PREFRANCE_NAME,MODE_PRIVATE);
+        edit=preferences.edit();
         binding.logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 Intent inext = new Intent(Teachers_DashBoard.this,Login_Activity.class);
+                edit.putBoolean(Keys.FLAG,false);
                 startActivity(inext);
             }
         });
